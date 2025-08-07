77e2a5787656b98c9eb05f49695ba41c8ef16c03
AmeerLala-source <laklaw100@gmail.com>
2025-08-07 14:57:53 +0300

    seed data

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    app.enableCors({ origin: 'http://localhost:3000' });  // allow your Next.js app
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
